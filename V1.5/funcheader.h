8510986ca1f5e9d370e220a5817116490e498ab5
assigin <117591072+assigin@users.noreply.github.com>
2023-05-31 12:31:20 +0300

    Add files via upload

---
+++ V1.5/funcheader.h
@@ -0,0 +1,124 @@
+#pragma once
+#pragma once
+#include "lib.h"
+class zmogus
+{
+protected:
+	string vardas, pavarde;
+public:
+	zmogus()
+	{
+		vardas = "vardenis";
+		pavarde = "pavardenis";
+	}
+	virtual void setVardas(string& v) { vardas = v; }
+	virtual void setPavarde(string& p) { pavarde = p; }
+
+	virtual ~zmogus() {};
+};
+class studentas : public zmogus
+{
+private:
+	vector<int> pazymiai;
+	int egzaminas;
+	double vid;
+	double med;
+	int suma;
+
+public:
+	studentas(const std::string& v = "vardas", const std::string& p = "pavarde") : zmogus() { vardas = v; pavarde = p; suma = 0; cout << "zmogus iskviestas" << endl; }
+	studentas(const studentas& stud)
+	{
+		cout << "copy konstruktorius" << endl;
+		vardas = stud.vardas;
+		pavarde = stud.pavarde;
+		pazymiai = stud.pazymiai;
+		egzaminas = stud.egzaminas;
+		vid = stud.vid;
+		med = stud.med;
+		suma = stud.suma;
+	}
+
+	studentas(const studentas&& stud) noexcept
+	{
+		cout << "move konstruktorius" << endl;
+		vardas = stud.vardas;
+		pavarde = stud.pavarde;
+		pazymiai = move(stud.pazymiai);
+		egzaminas = stud.egzaminas;
+		vid = stud.vid;
+		med = stud.med;
+		suma = stud.suma;
+		stud.~studentas();
+	}
+	studentas& operator=(studentas& stud_2)
+	{
+		cout << "copy darbas" << endl;
+		if (this != &stud_2)
+		{
+			vardas = stud_2.vardas;
+			pavarde = stud_2.pavarde;
+			pazymiai = stud_2.pazymiai;
+			egzaminas = stud_2.egzaminas;
+			vid = stud_2.vid;
+			med = stud_2.med;
+			suma = stud_2.suma;
+		}
+		return *this;
+	}
+	studentas& operator=(studentas&& stud_2) noexcept
+	{
+		cout << "move darbas" << endl;
+		if (this != &stud_2)
+		{
+			vardas = move(stud_2.vardas);
+			pavarde = move(stud_2.pavarde);
+			pazymiai = move(stud_2.pazymiai);
+			egzaminas = move(stud_2.egzaminas);
+			vid = move(stud_2.vid);
+			med = move(stud_2.med);
+			suma = move(stud_2.suma);
+		}
+		return *this;
+	}
+	friend std::ostream& operator<<(std::ostream& os, const studentas& stud) {
+		os << "Vardas: " << stud.vardas << std::endl;
+		os << "Pavarde: " << stud.pavarde << std::endl;
+		// Output other member variables as needed
+		return os;
+	}
+	friend istream& operator>>(istream& input, studentas& studentas) {
+		input >> studentas.vardas >> studentas.pavarde >> studentas.egzaminas;
+		return input;
+	}
+
+	void setVardas(string vard) { vardas = vard; }
+	void setPavarde(string pavard) { pavarde = pavard; }
+	void setPazymiai(int paz) { pazymiai.push_back(paz); }
+	void clearPazymiai() { pazymiai.clear(); }
+	void setEgzaminas(double egz) { egzaminas = egz; }
+	void setMed(double medi) { med = medi; }
+	void setSuma(int sum) { suma = sum + suma; }
+	void clearSuma() { suma = 0; }
+	void setVid(double vidu) { vid = vidu; }
+
+	string getVardas() { return vardas; }
+	string getPavarde() { return pavarde; }
+	vector<int> getPazymiai() { return pazymiai; }
+	double getEgzaminas() { return egzaminas; }
+	double getMed() { return med; }
+	int getSuma() { return suma; }
+	double getVid() { return vid; }
+};
+
+void ivedimas(studentas& temp);
+void galutinis(studentas& temp);
+void mediana(studentas& temp);
+void skaitymas_is_failo(string read_vardas, vector<studentas>& mas, studentas& temp);
+void spausdinimas(vector<studentas> mas);
+bool rusiavimas(studentas& pirm, studentas& kit);
+void visiskas_generavimas(studentas& temp, int& x, int& n);
+bool rusiavimas_2(studentas& pirm, studentas& kit);
+bool rusiavimas_3(studentas& pirm, studentas& kit);
+void spaus(vector<studentas>& mas, char b, string file_name);
+void skirstymas(vector<studentas>& zino, vector<studentas>& nezino, char& b, vector<studentas>& mas);
---
+++ V1.5/lib.h
@@ -0,0 +1,34 @@
+#pragma once
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include <vector>
+#include <limits>
+#include <algorithm>
+#include <cstdio>
+#include <random>
+#include <fstream>
+#include <chrono>
+#include <list>
+#include<deque>
+#include <iterator>
+
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::right;
+using std::setprecision;
+using std::setw;
+using std::string;
+using std::copy;
+using std::vector;
+using std::fixed;
+using std::sort;
+using std::ifstream;
+using std::ofstream;
+using std::getline;
+using std::to_string;
+using std::list;
+using std::deque;
